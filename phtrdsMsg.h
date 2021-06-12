39792685b83c9995e0abff7f7d32cd2faf92616d
juaca2009 <juaca2009@gmail.com>
2021-06-12 16:55:32 -0500

    definicion colas

---
+++ phtrdsMsg.h
@@ -0,0 +1,21 @@
+#include <pthread.h>
+#include <semaphore.h>
+#include "pMaquinasConf.h"
+
+#define CRM 0
+
+typedef struct{
+    Smensaje buffer[BUFSIZE];
+    int bufin;
+    int bufout;
+    pthread_mutex_t buffer_lock;
+    sem_t items;
+    sem_t slots;
+} Qmensaje;
+
+extern Qmensaje queque [NUM_COLAS];
+
+void iniciarColas(void);
+void destruirColas(void);
+void enviarMensaje(Qmensaje *_Qmensaje, Smensaje _mensaje);
+Smensaje recibirMensaje(Qmensaje *_Qmensaje);
